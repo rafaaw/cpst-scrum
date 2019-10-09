de47a9cc91d4f567a27dd2888eff852961bc74b3
Rafael Chaves de Souza <rafaelnep@hotmail.com>
2019-10-08 21:14:01 -0300

    innitial commit

---
+++ src/main/java/com/cpst/CooperstandardScrumApplication.java
@@ -0,0 +1,13 @@
+package com.cpst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperstandardScrumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooperstandardScrumApplication.class, args);
+	}
+
+}
